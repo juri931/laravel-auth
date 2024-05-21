71bc7d84b2c9462c1a255e175ad35c893837f676
juri931 <impa.mat@hotmail.it>
2024-05-21 16:39:12 +0200

    "Removed welcome.blade.php file and updated routes in web.php"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+                    ->prefix('admin')
+                    ->name('admin.')
+                    ->group(function(){
+                        Route::get('/', [DashboardController::class, 'index'])->name('home');
+                    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
