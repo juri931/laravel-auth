a1e6792e83fca9064e0f331dc31ede8ffdb8229a
juri931 <impa.mat@hotmail.it>
2024-05-22 16:12:43 +0200

    "Updated method signature of run() in ProjectTableSeeder to accept a Faker parameter."

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -11,7 +11,7 @@ class ProjectTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
     }
