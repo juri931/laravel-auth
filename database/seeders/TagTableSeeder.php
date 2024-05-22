c4c461407bc08e58a43ae5fa8d068019031fd954
juri931 <impa.mat@hotmail.it>
2024-05-22 16:52:11 +0200

    "Added TagTableSeeder to seed tags table with initial data"

--- database/seeders/TagTableSeeder.php
+++ database/seeders/TagTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use App\Functions\Helper;
 
 class TagTableSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class TagTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = ['Front End', 'Back End', 'Design', 'UX', 'Laravel', 'VueJS', 'Angular', 'React'];
+        foreach ($data as $item) {
+            $new_item = new Tag();
+            $new_item->name = $item;
+            $new_item->slug = Helper::generateSlug($item, Tag::class);
+            $new_item->save();
+        }
     }
 }
