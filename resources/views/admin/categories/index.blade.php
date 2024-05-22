6267f203eaba2258565266904141e658095e8b80
juri931 <impa.mat@hotmail.it>
2024-05-22 17:45:23 +0200

    "Added CRUD table styling and functionality to categories index page"

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -2,4 +2,32 @@
 
 @section('content')
     <h2>Categorie</h2>
+
+    <div class="d-flex my4">
+        <input class="form-control me-2" type="search" placeholder="Nuova Categoria" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Invia</button>
+    </div>
+
+    <table class="table crud-table">
+        <thead>
+            <tr>
+                <th scope="col">Categoria</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $category)
+                <tr>
+                    <td>
+                        <input type="text" value="{{ $category->name }}">
+                    </td>
+
+                    <td>
+                        <button class="btn btn-warning"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
