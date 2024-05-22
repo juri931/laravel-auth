b904171c587815578605ff4d7c17fad282bd0fb1
juri931 <impa.mat@hotmail.it>
2024-05-22 17:35:00 +0200

    "Added Category model and route to CategoryController, updated aside.blade.php to link to categories index route"

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -8,7 +8,7 @@
                 <a href="#"><i class="fa-solid fa-circle-plus"></i>Nuovo Progetto</a>
             </li>
             <li>
-                <a href="#"><i class="fa-solid fa-list-check"></i>Categorie</a>
+                <a href="{{ route('admin.categories.index') }}"><i class="fa-solid fa-list-check"></i>Categorie</a>
             </li>
             <li>
                 <a href="#"><i class="fa-solid fa-terminal"></i>Tags</a>
