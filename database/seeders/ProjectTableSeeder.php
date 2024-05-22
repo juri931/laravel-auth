5e7a7794ad18dace4f27acb7cc88be15fb8a0520
juri931 <impa.mat@hotmail.it>
2024-05-22 16:20:46 +0200

    "Added use statement for App\Models\Project and removed newline at end of file."

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Project;
 
 class ProjectTableSeeder extends Seeder
 {
