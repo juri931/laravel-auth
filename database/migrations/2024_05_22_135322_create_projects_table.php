4d0ad61f61a78bf533c02037ad382250060801fe
juri931 <impa.mat@hotmail.it>
2024-05-22 15:58:50 +0200

    "Added columns to projects table in migration"

--- database/migrations/2024_05_22_135322_create_projects_table.php
+++ database/migrations/2024_05_22_135322_create_projects_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('slug', 100)->unique();
+            $table->string('category');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->date('created');
             $table->timestamps();
         });
     }
