a86e624ed575d739785af1719bfd73683032ecbd
juri931 <impa.mat@hotmail.it>
2024-05-23 14:49:59 +0200

    "Updated app.scss with new styles for header and aside, and updated aside.blade.php with route links"

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,10 +2,10 @@
     <nav>
         <ul>
             <li>
-                <a href="#"><i class="fa-solid fa-terminal"></i>Projects</a>
+                <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-terminal"></i>Projects</a>
             </li>
             <li>
-                <a href="#"><i class="fa-solid fa-circle-plus"></i>Nuovo Progetto</a>
+                <a href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-circle-plus"></i>Nuovo Progetto</a>
             </li>
             <li>
                 <a href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-list-check"></i>Tecnologie</a>
